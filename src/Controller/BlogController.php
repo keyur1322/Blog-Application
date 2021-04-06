88c4a4e9754b5b23487bcf9ef6094c5209ff5e41
keyur1322 <44034672+keyur1322@users.noreply.github.com>
2021-04-06 18:58:58 +0530

    changes

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -108,47 +108,47 @@ class BlogController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/{id}", name="blog_show", methods={"GET"})
-     */
-    public function show(Blog $blog): Response
-    {
-        return $this->render('blog/show.html.twig', [
-            'blog' => $blog,
-        ]);
-    }
-
-    /**
-     * @Route("/{id}/edit", name="blog_edit", methods={"GET","POST"})
-     */
-    public function edit(Request $request, Blog $blog): Response
-    {
-        $form = $this->createForm(BlogType::class, $blog);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('blog_index');
-        }
-
-        return $this->render('blog/edit.html.twig', [
-            'blog' => $blog,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="blog_delete", methods={"POST"})
-     */
-    public function delete(Request $request, Blog $blog): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$blog->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($blog);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('blog_index');
-    }
+//    /**
+//     * @Route("/{id}", name="blog_show", methods={"GET"})
+//     */
+//    public function show(Blog $blog): Response
+//    {
+//        return $this->render('blog/show.html.twig', [
+//            'blog' => $blog,
+//        ]);
+//    }
+//
+//    /**
+//     * @Route("/{id}/edit", name="blog_edit", methods={"GET","POST"})
+//     */
+//    public function edit(Request $request, Blog $blog): Response
+//    {
+//        $form = $this->createForm(BlogType::class, $blog);
+//        $form->handleRequest($request);
+//
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            $this->getDoctrine()->getManager()->flush();
+//
+//            return $this->redirectToRoute('blog_index');
+//        }
+//
+//        return $this->render('blog/edit.html.twig', [
+//            'blog' => $blog,
+//            'form' => $form->createView(),
+//        ]);
+//    }
+//
+//    /**
+//     * @Route("/{id}", name="blog_delete", methods={"POST"})
+//     */
+//    public function delete(Request $request, Blog $blog): Response
+//    {
+//        if ($this->isCsrfTokenValid('delete'.$blog->getId(), $request->request->get('_token'))) {
+//            $entityManager = $this->getDoctrine()->getManager();
+//            $entityManager->remove($blog);
+//            $entityManager->flush();
+//        }
+//
+//        return $this->redirectToRoute('blog_index');
+//    }
 }
--- tests/Controller/BlogControllerTest.php
+++ tests/Controller/BlogControllerTest.php
@@ -7,91 +7,103 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class BlogControllerTest extends WebTestCase
 {
+    /** index method.
+     *
+     */
     public function testShowAll()
     {
         $client = static::createClient();
 
-        $client->request('GET', '/blog/');
+        $crawler = $client->request('GET', '/blog/');
+
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals(3, $crawler->filter('#testul > li')->count());
     }
 
-    public function testShowCategory()
-    {
-        $client = static::createClient();
-
-        $client->request('GET', '/blog/blog_category/{variable}');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-    }
-
-    public function testShowBlogDetails()
-    {
-        $client = static::createClient();
-
-        $client->request('GET', '/blog/blog_details/{blogid}');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-    }
-
-    public function provideUrls(): array
-    {
-        return [
-          ['GET', '/blog/blog_filter_date'],
-          ['POST', '/blog/blog_filter_date'],
-        ];
-    }
-
-    /**
-     * @dataProvider provideUrls
+    /** new method.
      *
-     * @param $method
-     * @param $url
      */
-    public function testBlogFilterDate($method, $url)
-    {
-        $client = static::createClient();
-
-        $client->request($method, $url);
-        $this->assertTrue($client->getResponse()->isSuccessful());
-    }
-
-    public function testAllUserBlog()
-    {
-        $client = static::createClient();
-
-        $client->request('GET', '/blog/all_user_blog');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-    }
-
     public function testAddBlog()
     {
         $client = static::createClient();
 
+        $userRepository = static::$container->get(UserRepository::class);
+
+        $testUser = $userRepository->findOneByEmail('john.doe@example.com');
+        $client->loginUser($testUser);
+
         $crawler = $client->request('POST', '/blog/new');
 
         $buttonCrawlerNode = $crawler->selectButton('blog');
-        $userRepository = static::$container->get(UserRepository::class);
 
-        // retrieve the test user
-        $testUser = $userRepository->findOneByEmail('john.doe@example.com');
-
-        // simulate $testUser being logged in
-        $client->loginUser($testUser);
-
-
-        // you can also pass an array of field values that overrides the default ones
         $form = $buttonCrawlerNode->form([
-            'blog[title]' => 'Fabien',
+            'blog[title]' => 'blog title',
             'blog[category]' => 'technology',
-            'blog[ShortDescription]' => 'Symfony rocks!',
-            'blog[LongDescription]' => 'Symfony rocks!',
+            'blog[ShortDescription]' => 'blog short description',
+            'blog[LongDescription]' => 'blog long description',
             'blog[image]' => 'D:\Symfony\Blog-application\public\img\91c9c0ddd91bf6ed2f3c07da295a00e3.jpg',
         ]);
 
-        //  $form['blog[image]']->upload('D:\Symfony\Blog-application\public\img\91c9c0ddd91bf6ed2f3c07da295a00e3.jpg');
-
         $client->submit($form);
 
-        $crawler = $client->request('GET', '/blog/all_user_blog/');
-//        $this->assertEquals(1, $crawler->filter('div.header > h2')->count());
-        $this->assertCount(6, $crawler->filter('h2'));
+        $crawler = $client->request('GET', '/blog/all_user_blog');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals(1, $crawler->filter('h1')->count());
+    }
+
+    /** blog_details method.
+     *
+     */
+    public function testShowBlogDetails()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/blog/blog_details/1');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertSelectorTextContains('#testh1', 'blog title');
+    }
+
+//    /**
+//     * @return string[][]
+//     */
+//    public function provideUrls(): array
+//    {
+//        return [
+//          ['GET', '/blog/blog_filter_date'],
+//          ['POST', '/blog/blog_filter_date'],
+//        ];
+//    }
+
+    /** filter_date method.
+     *
+     */
+    public function testBlogFilterDate()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/blog/blog_filter_date');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertSelectorTextContains('#testnofound', 'No Blog Found.');
+    }
+
+    /** all_user_blog method.
+     *
+     */
+    public function testAllUserBlog()
+    {
+        $client = static::createClient();
+
+        $userRepository = static::$container->get(UserRepository::class);
+
+        $testUser = $userRepository->findOneByEmail('john.doe@example.com');
+        $client->loginUser($testUser);
+
+        $crawler = $client->request('GET', '/blog/all_user_blog');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertGreaterThan(0, $crawler->filter('.testpost')->count());
     }
 }
